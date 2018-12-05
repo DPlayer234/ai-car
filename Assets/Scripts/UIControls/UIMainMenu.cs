6e7453ba442feff07a757627c4eae6aa989f446b
DPlayer234 <dplayer.235@gmail.com>
2018-12-05 17:16:21 +0100

    Fixed Road Textures and added new genome codes

---
+++ Assets/Scripts/UIControls/UIMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DPlay.AICar.UIControls
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        public void ExitApplication()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
